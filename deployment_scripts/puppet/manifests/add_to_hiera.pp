c86b8f46d73b9d42bf1e06a38e7a1e7acfb9bb8c
Aleksandr Didenko <adidenko@mirantis.com>
2016-03-14 18:30:10 +0100

    Added hiera conf task

---
+++ deployment_scripts/puppet/manifests/add_to_hiera.pp
@@ -0,0 +1,28 @@
+notice('MODULAR: detach-haproxy/add_to_hiera.pp')
+
+$plugin_name      = 'detach_haproxy'
+$network_metadata = hiera_hash('network_metadata', {})
+$haproxy_nodes    = get_nodes_hash_by_roles($network_metadata, ['standalone-haproxy'])
+
+if size($haproxy_nodes) != 1 {
+  fail('There should be only one standalone haproxy node')
+}
+
+$mgmt = values(get_node_to_ipaddr_map_by_network_role($haproxy_nodes, 'mgmt/vip'))
+$mgmt_ip = $mgmt[0]
+
+$public = values(get_node_to_ipaddr_map_by_network_role($haproxy_nodes, 'public/vip'))
+$public_ip = $public[0]
+
+file {"/etc/hiera/plugins/${plugin_name}.yaml":
+  ensure  => file,
+    content => inline_template("# Created by puppet, please do not edit manually
+network_metadata:
+  vips:
+    management:
+      ipaddr: <%= @mgmt_ip %>
+    public:
+      ipaddr: <%= @public_ip %>
+")
+}
+
